ef0f9e6acb1b2ddf02cafd02dc069eaf5f1c15b2
al8n <scygliu1@gmail.com>
2025-10-18 19:20:57 +1300

    wip

--- smear/tests/parser/graphql/err/err_0001_directive_definition_missing_location.rs
+++ smear/tests/parser/graphql/err/err_0001_directive_definition_missing_location.rs
@@ -1,9 +1,11 @@
-use smear::parser::graphql::ast::{Document, ParseStr};
+
 
 
 const ALL: &str = include_str!("../../../fixtures/parser/graphql/err/0001_directive_definition_missing_location.graphql");
 
+#[cfg(feature = "graphql")]
 #[test]
 fn directive_definition_missing_location() {
+  use smear::parser::graphql::ast::{Document, ParseStr};
   let _ = Document::<&str>::parse_str(ALL).into_result().unwrap_err();
 }
--- smear/tests/parser/graphql/err/err_0062_kitchen_sink_ccn.rs
+++ smear/tests/parser/graphql/err/err_0062_kitchen_sink_ccn.rs
@@ -3,7 +3,7 @@ use smear::parser::graphql::ast::{Document, ParseStr};
 
 const ALL: &str = include_str!("../../../fixtures/parser/graphql/err/0062_kitchen_sink_ccn.graphql");
 
-
+#[cfg(feature = "graphql")]
 #[test]
 fn kitchen_sink_ccn() {
   let _err = Document::<&str>::parse_str(ALL).into_result().unwrap_err();
