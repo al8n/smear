f43e696995ba97df32d983d5fb17d74b4baed440
al8n <scygliu1@gmail.com>
2026-07-09 12:42:54 +1200

    feat(parser-next): scaffold the atomic-combinator crate

---
+++ smear-parser-next/src/lib.rs
@@ -0,0 +1,17 @@
+#![doc = include_str!("../README.md")]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
+#![deny(missing_docs)]
+
+#[cfg(not(feature = "std"))]
+extern crate alloc as std;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+/// Re-exported lexer crate.
+pub use smear_lexer as lexer;
+
+/// Re-exported scaffold crate.
+pub use smear_scaffold as scaffold;
